19e6508132deced889af98b6dbeb4add1273fc1a
dominique maack <dominiquemaack@gmail.com>
2019-09-11 15:59:52 -0600

    image and information displaying

--- src/Components/PhotoList.js
+++ src/Components/PhotoList.js
@@ -1,32 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import PhotoCard from './PhotoCard';
+// import PhotoCard from './PhotoCard';
 import axios from 'axios';
 
 
 function PhotoList() {
-    const [photos, setPhotos] = useState([]);
+    const [photo, setPhoto] = useState([]);
 
     useEffect(() => {
         axios
-        .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14`)
+        .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
         .then(response => {
             const newPhoto = response.data;
             console.log('NASA photo of the day', newPhoto);
-            setPhotos(newPhoto);
+            setPhoto(newPhoto);
         })
         .catch(error => {
             console.log('The data was not returned', error);
         });
     }, []);
+
     return( 
-        <div className='container'>
-            {photos.map(photo => {
-                return(
-                    <PhotoCard key={photos.id} title={photos.title} date={photos.date} img={photos.hdurl} copyright={photos.copyright} explanation={photos.explanation} />
-                )
-            })}
+        <div className='photo_container'>
+            <img className='photo_of_day' alt='astronomic scene in outer space' src={photo.url} />
+            <p className='title'>{photo.title}</p>
+            <p className='copyright'>Image Credit & Copyright: {photo.copyright}</p>
+            <p className='explanation'>Explanation: {photo.explanation}</p>
         </div>
-    )
-}
+    );
+};
 
 export default PhotoList;
+
