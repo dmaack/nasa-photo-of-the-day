ae9cc01b538ae6311a80e187d6f24d3d5920c510
dominique maack <dominiquemaack@gmail.com>
2019-09-12 09:12:21 -0600

    Added date selector

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "./App.css";
-import PhotoList from './Components/PhotoList';
+import PhotoCard from './Components/PhotoCard';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        
-      </div>
+      <div className='card-container'>
       <h1>NASA Astronomy Photo of the Day</h1>
       <p>Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.</p>
-      <PhotoList />
+      <PhotoCard />
+      </div>
     </div>
   );
 }
---
+++ src/Components/PhotoCard.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+function PhotoCard() {
+    const [photo, setPhoto] = useState({});
+    const [date, setDate] = useState('2019-09-12');
+
+    useEffect(() => {
+        axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
+        .then(response => {
+            const newPhoto = response.data;
+            console.log('NASA photo of the day', newPhoto);
+            setPhoto(newPhoto);
+        })
+        .catch(error => {
+            console.log('The data was not returned', error);
+        });
+    }, [date]);
+
+    return( 
+        <div className='photo_container'>
+            <h2>Select the date for new photo:</h2>
+            <input type='date' onChange={e => setDate(e.target.value)}  />
+            <img className='photo_of_day' alt='astronomic scene in outer space' src={photo.url} />
+            <h3 className='title'>{photo.title}</h3>
+            <h3 className='copyright'>Image Credit & Copyright: {photo.copyright}</h3>
+            <p className='explanation'><strong>Explanation: </strong> {photo.explanation}</p>
+            <h3>Tomorrow's photo: </h3>
+        </div>
+    );
+};
+
+export default PhotoCard;
+
