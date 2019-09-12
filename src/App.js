df63a74ac51b8b4854b61df6ca34f7f546bfbed2
dominique maack <dominiquemaack@gmail.com>
2019-09-12 15:13:27 -0600

    Added some reactstrap styling = card component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "./App.css";
 import PhotoCard from './Components/PhotoCard';
+import logo from './nasaLogo.png'
 
 function App() {
   return (
     <div className="App">
     
       <div className='App-header'>
-        <img className='App-logo' src='./nasaLogo.png' alt='NASA Logo'/>
+        <img className='App-logo' src={logo} alt='NASA Logo'/>
         <h1>NASA Astronomy Photo of the Day</h1>
         <p>Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.</p>
       </div>
 
       <PhotoCard />
-      
+
     </div>
   );
 }
--- src/Components/PhotoCard.js
+++ src/Components/PhotoCard.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {
+    Card, CardText, CardBody, CardLink,
+    CardTitle, CardSubtitle, Input, FormGroup, Label} from 'reactstrap';
 
 
 function PhotoCard() {
@@ -20,23 +23,91 @@ function PhotoCard() {
     }, [date]);
 
     return( 
-        <div className='photo_container'>
-            <div className='date_selector'>
-                <h2>Select the date for new photo:</h2>
-                <input type='date' onChange={e => setDate(e.target.value)}  />
-            </div>
-           
-            <img className='photo_of_day' alt='astronomic scene in outer space' src={photo.url} />
-            
-            <div className='photo_info'>
-                <h3 className='title'>{photo.title}</h3>
-                <h3 className='copyright'>Image Credit & Copyright: {photo.copyright}</h3>
-                <p className='explanation'><strong>Explanation: </strong> {photo.explanation}</p>
-                <h3>Tomorrow's photo: </h3>
-            </div>
-        </div>
+        <div>
+        <Card>
+          <CardBody>
+            <CardTitle>Need to Pick my Card's Title</CardTitle>
+            <CardSubtitle>Photo's Subtitle</CardSubtitle>
+            <FormGroup>
+            <Label for="exampleDate">Select Your Date:</Label>
+            <Input
+              type="date"
+              name="date"
+              id="exampleDate"
+              placeholder="date placeholder"
+              onChange={e => setDate(e.target.value)}
+            />
+            </FormGroup>
+          </CardBody>
+           <img width="100%" src={photo.url} alt="Card image cap" />
+          <CardBody>
+            <CardText>{photo.title}</CardText>
+            <CardText>Image Credit & Copyright: {photo.copyright}</CardText>
+            <CardText><strong>Explanation: </strong> {photo.explanation}</CardText>
+  
+            <CardLink href="#">Card Link</CardLink>
+            <CardLink href="#">Another Link</CardLink>
+          </CardBody>
+        </Card>
+      </div>
     );
-};
+  };
+
+
+
+
+//         <div className='photo_container'>
+//             <div className='date_selector'>
+//                 <h2>Select the date for new photo:</h2>
+//                 <input type='date' onChange={e => setDate(e.target.value)}  />
+//             </div>
+           
+//             <img className='photo_of_day' alt='astronomic scene in outer space' src={photo.url} />
+
+//             <div className='photo_info'>
+//                 <h3 className='title'>{photo.title}</h3>
+//                 <h3 className='copyright'>Image Credit & Copyright: {photo.copyright}</h3>
+//                 <p className='explanation'><strong>Explanation: </strong> {photo.explanation}</p>
+//                 <h3>Tomorrow's photo: </h3>
+//             </div>
+//         </div>
+//     );
+// };
 
 export default PhotoCard;
 
+
+
+// const Example = (props) => {
+//   return (
+//     <div>
+//       <Card>
+//         <CardBody>
+//           <CardTitle>Need to Pick my Card's Title</CardTitle>
+//           <CardSubtitle>Photo's Subtitle</CardSubtitle>
+//           <FormGroup>
+//           <Label for="exampleDate">Select Your Date:</Label>
+//           <Input
+//             type="date"
+//             name="date"
+//             id="exampleDate"
+//             placeholder="date placeholder"
+//             onChange={e => setDate(e.target.value)}
+//           />
+//           </FormGroup>
+//         </CardBody>
+//          <img width="100%" src={photo.url} alt="Card image cap" />
+//         <CardBody>
+//           <CardText>{photo.title}</CardText>
+//           <CardText>Image Credit & Copyright: {photo.copyright}</CardText>
+//           <CardText><strong>Explanation: </strong> {photo.explanation}</CardText>
+
+//           <CardLink href="#">Card Link</CardLink>
+//           <CardLink href="#">Another Link</CardLink>
+//         </CardBody>
+//       </Card>
+//     </div>
+//   );
+// };
+
+// export default Example;
